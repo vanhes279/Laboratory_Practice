8fd4a89e2e667880c3ff8299279a6f5fd2260ec9
Ivan Orlov <www.orlovivan1@gmail.com>
2025-11-14 18:24:58 +0700

    s

--- Core/Src/init.c
+++ Core/Src/init.c
@@ -13,42 +13,42 @@ CLEAR_BIT(RCC->CR, RCC_CR_HSEON | RCC_CR_CSSON);
 while (READ_BIT(RCC->CR, RCC_CR_HSERDY) != RESET);
 CLEAR_BIT(RCC->CR, RCC_CR_HSEBYP);
 /* Настройка главного регистра RCC */
- SET_BIT(RCC->CR, RCC_CR_HSEON);                                                    //Запускаем внешний кварцевыйрезонатор
- while(READ_BIT(RCC->CR, RCC_CR_HSERDY) == RESET);                                  //Ждём пока он запустится
- CLEAR_BIT(RCC->CR, RCC_CR_HSEBYP);                                                 //Сбросим бит байпаса в 0
- SET_BIT(RCC->CR, RCC_CR_CSSON);                                                    //Запустим Clock detector
+SET_BIT(RCC->CR, RCC_CR_HSEON);                                                    //Запускаем внешний кварцевыйрезонатор
+while(READ_BIT(RCC->CR, RCC_CR_HSERDY) == RESET);                                  //Ждём пока он запустится
+CLEAR_BIT(RCC->CR, RCC_CR_HSEBYP);                                                 //Сбросим бит байпаса в 0
+SET_BIT(RCC->CR, RCC_CR_CSSON);                                                    //Запустим Clock detector
 
 /* Натсройка регистров PLL
 * Предварительная очистка регистра
 * Включаем работу PLL*/
- CLEAR_REG(RCC->PLLCFGR);
- SET_BIT(RCC->PLLCFGR, RCC_PLLCFGR_PLLSRC_HSE);
- MODIFY_REG(RCC->PLLCFGR, RCC_PLLCFGR_PLLM, RCC_PLLCFGR_PLLM_2);
- MODIFY_REG(RCC->PLLCFGR, RCC_PLLCFGR_PLLN_Msk, RCC_PLLCFGR_PLLN_2 |
+CLEAR_REG(RCC->PLLCFGR);
+SET_BIT(RCC->PLLCFGR, RCC_PLLCFGR_PLLSRC_HSE);
+MODIFY_REG(RCC->PLLCFGR, RCC_PLLCFGR_PLLM, RCC_PLLCFGR_PLLM_2);
+MODIFY_REG(RCC->PLLCFGR, RCC_PLLCFGR_PLLN_Msk, RCC_PLLCFGR_PLLN_2 |
 RCC_PLLCFGR_PLLN_4 | RCC_PLLCFGR_PLLN_5 | RCC_PLLCFGR_PLLN_7);
- CLEAR_BIT(RCC->PLLCFGR, RCC_PLLCFGR_PLLP_Msk);                                     //Настраивае мпредделитель получившейся частоты после умножения. Иными словами,получаем итоговую частоту PLL
+CLEAR_BIT(RCC->PLLCFGR, RCC_PLLCFGR_PLLP_Msk);                                     //Настраивае мпредделитель получившейся частоты после умножения. Иными словами,получаем итоговую частоту PLL
 
- SET_BIT(RCC->CR, RCC_CR_PLLON);                                                    //Запустим PLL
- while(READ_BIT(RCC->CR, RCC_CR_PLLRDY));                                           //Ждём запуска PLL84
+SET_BIT(RCC->CR, RCC_CR_PLLON);                                                    //Запустим PLL
+while(READ_BIT(RCC->CR, RCC_CR_PLLRDY));                                           //Ждём запуска PLL84
 /* Настройка основных конфигураций RCC
 * В качетсве системных часов выбираем выход PLL
 * Настраиваем предделители шин AHB и APB
 * Настраиваем выходы MCO1 и MCO2 для внешней оценки настроенной
 системы тактирования*/
- MODIFY_REG(RCC->CFGR, RCC_CFGR_SW, RCC_CFGR_SW_PLL);                               //Выбираем PLLв качестве System Clock
- MODIFY_REG(RCC->CFGR, RCC_CFGR_HPRE, RCC_CFGR_HPRE_DIV1);
+MODIFY_REG(RCC->CFGR, RCC_CFGR_SW, RCC_CFGR_SW_PLL);                               //Выбираем PLLв качестве System Clock
+MODIFY_REG(RCC->CFGR, RCC_CFGR_HPRE, RCC_CFGR_HPRE_DIV1);
 //Предделитель AHB, без делителя
- MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE1, RCC_CFGR_PPRE1_DIV4);
+MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE1, RCC_CFGR_PPRE1_DIV4);
 //Предделитель APВ1, делим на 4
- MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE2, RCC_CFGR_PPRE2_DIV2);
+MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE2, RCC_CFGR_PPRE2_DIV2);
 //Предделитель APВ2, делим на 2
- MODIFY_REG(RCC->CFGR, RCC_CFGR_MCO2PRE, RCC_CFGR_MCO2PRE_Msk);
+MODIFY_REG(RCC->CFGR, RCC_CFGR_MCO2PRE, RCC_CFGR_MCO2PRE_Msk);
 //Предделитель на выходе MCO2 (PC9) = 5
- CLEAR_BIT(RCC->CFGR, RCC_CFGR_MCO2);                                               //Настраиваем на выход MCO2 -System clock
+CLEAR_BIT(RCC->CFGR, RCC_CFGR_MCO2);                                               //Настраиваем на выход MCO2 -System clock
 /* Настройка задержки внутренней памяти
 * Выставление битов LATENCY регистра FLASH_ACR в позицию 5SW (6 CPU cycles).
 * Данная настройка необходима при увеличении системной частоты тактирования свыше 20 МГц. Таблица 12 RM0090*/
- MODIFY_REG(FLASH->ACR, FLASH_ACR_LATENCY, FLASH_ACR_LATENCY_5WS);
+MODIFY_REG(FLASH->ACR, FLASH_ACR_LATENCY, FLASH_ACR_LATENCY_5WS);
 }
 
 // Функция инициализации GPIO
