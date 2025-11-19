6975da0ff452ef4c699d7f88764c4e5770b86f7f
Ivan Orlov <www.orlovivan1@gmail.com>
2025-11-19 21:07:40 +0700

    ossci

--- Core/Src/init.c
+++ Core/Src/init.c
@@ -27,25 +27,7 @@ MODIFY_REG(RCC->PLLCFGR, RCC_PLLCFGR_PLLN_Msk, RCC_PLLCFGR_PLLN_2 |RCC_PLLCFGR_P
 CLEAR_BIT(RCC->PLLCFGR, RCC_PLLCFGR_PLLP_Msk);                                     //Настраивае мпредделитель получившейся частоты после умножения. Иными словами,получаем итоговую частоту PLL
 SET_BIT(RCC->CR, RCC_CR_PLLON);                                                    //Запустим PLL
 while(READ_BIT(RCC->CR, RCC_CR_PLLRDY));                                           //Ждём запуска PLL84
-/* Настройка основных конфигураций RCC
-* В качетсве системных часов выбираем выход PLL
-* Настраиваем предделители шин AHB и APB
-* Настраиваем выходы MCO1 и MCO2 для внешней оценки настроенной
-системы тактирования*/
-MODIFY_REG(RCC->CFGR, RCC_CFGR_SW, RCC_CFGR_SW_PLL);                               //Выбираем PLLв качестве System Clock
-MODIFY_REG(RCC->CFGR, RCC_CFGR_HPRE, RCC_CFGR_HPRE_DIV1);
-//Предделитель AHB, без делителя
-MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE1, RCC_CFGR_PPRE1_DIV4);
-//Предделитель APВ1, делим на 4
-MODIFY_REG(RCC->CFGR, RCC_CFGR_PPRE2, RCC_CFGR_PPRE2_DIV2);
-//Предделитель APВ2, делим на 2
-MODIFY_REG(RCC->CFGR, RCC_CFGR_MCO2PRE, RCC_CFGR_MCO2PRE_Msk);
-//Предделитель на выходе MCO2 (P9) = 5
-CLEAR_BIT(RCC->CFGR, RCC_CFGR_MCO2);                                               //Настраиваем на выход MCO2 -System clock
-/* Настройка задержки внутренней памяти
-* Выставление битов LATENCY регистра FLASH_ACR в позицию 5SW (6 CPU cycles).
-* Данная настройка необходима при увеличении системной частоты тактирования свыше 20 МГц. Таблица 12 RM0090*/
-MODIFY_REG(FLASH->ACR, FLASH_ACR_LATENCY, FLASH_ACR_LATENCY_5WS);
+
 }
 
 // Функция инициализации GPIO
@@ -130,6 +112,6 @@ void SysTick_Init(void)
     SET_BIT(SysTick->CTRL, SysTick_CTRL_TICKINT_Msk);                                     //Разрешаем прерывание по системному таймеру
     SET_BIT(SysTick->CTRL, SysTick_CTRL_CLKSOURCE_Msk);                                   //Источник тактирования будет идти из AHB без деления
     MODIFY_REG(SysTick->LOAD, SysTick_LOAD_RELOAD_Msk,179999 << SysTick_LOAD_RELOAD_Pos); //Будет тикать с частотой 1 кГц и вызывать прерывания
-    MODIFY_REG(SysTick->VAL, SysTick_VAL_CURRENT_Msk,0 << SysTick_VAL_CURRENT_Pos);       //Начнём считать со значения 0
+    MODIFY_REG(SysTick->VAL, SysTick_VAL_CURRENT_Msk,179999 << SysTick_VAL_CURRENT_Pos);       //Начнём считать со значения 0
     SET_BIT(SysTick->CTRL, SysTick_CTRL_ENABLE_Msk);                                      //Включим счётчик
 }
